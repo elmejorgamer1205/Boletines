05482d94071b3198c3646d7d7ffc33ed9d0de81b
Carlos home <elmejorgamer1205@gmail.com>
2025-09-25 18:18:59 +0200

    boletines

---
+++ Boletin1/src/Ejercicio6.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe un número:");
+        int n1 = sc.nextInt();
+        System.out.println("Escribe otro número:");
+        int n2 = sc.nextInt();
+
+        int producto = 0;
+
+        for (int i = 0; i < n2; i++) {
+            producto += n1;
+        }
+
+        System.out.println("El producto es: " + producto);
+    }
+}
