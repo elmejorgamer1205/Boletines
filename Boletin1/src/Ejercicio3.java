ff60ff8bdd72bade96fa64a115f7c4b1d904b077
Carlos <elmejorgamer1205@gmail.com>
2025-09-25 09:23:50 +0200

    Primeros ficheros boletin 1

--- Boletin1/src/Ejercicio3.java
+++ Boletin1/src/Ejercicio3.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Ejercicio3 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe un numero:");
+        int num = sc.nextInt();
+        if (num <= 0) {
+            System.out.println("Elija otro numero mayor que 0");
+            num = sc.nextInt();
+        }
+        if (num % 2 == 0) {
+            System.out.println("tu numero es par");
+        }
+        if (num % 3 == 0) {
+            System.out.println("tu numero es impar");
+        }
     }
 }
