da0d00b8932356583c49cf099a76faade3d8b1b9
Carlos home <elmejorgamer1205@gmail.com>
2025-09-25 18:02:57 +0200

    boletines

--- Boletin1/src/Ejercicio3.java
+++ Boletin1/src/Ejercicio3.java
@@ -8,12 +8,14 @@ public class Ejercicio3 {
         if (num <= 0) {
             System.out.println("Elija otro número mayor que 0");
             num = sc.nextInt();
-        }
-        if (num % 2 == 0) {
-            System.out.println("tu número es par");
-        }
-        if (num % 3 == 0) {
-            System.out.println("tu número es impar");
+            if (num % 2 == 0) {
+                System.out.println("tu número es par");
+            } else if (num % 3 == 0) {
+                System.out.println("tu número es divisible entre 3");
+            } else {
+                System.out.println("tu número es impar");
+            }
         }
     }
 }
+
