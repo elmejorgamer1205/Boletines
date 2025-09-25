1515e252403a825dd3f7cac2942ef05904bfa821
Carlos <elmejorgamer1205@gmail.com>
2025-09-25 13:19:59 +0200

    Primeros ficheros boletin 1

--- Boletin1/src/Ejercicio4.java
+++ Boletin1/src/Ejercicio4.java
@@ -5,6 +5,15 @@ public class Ejercicio4 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Escribe un número positivo:");
         int num = sc.nextInt();
+        int suma = 0;
 
+        while(num < 0){
+            System.out.println("El número no es valido, pon uno positivo");
+            num = sc.nextInt();
+        }
+        for (int i = 0; i <= num ; i++) {
+           suma = suma +i;
+        }
+        System.out.println("La suma de todos los número que hay entre el 0 y el "+ num +" es "+suma);
     }
 }
---
+++ Boletin1/src/Ejercicio5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe un número positivo:");
+        int num = sc.nextInt();
+
+        int negativo = -1;
+
+        {
+        }
+    }
+}
