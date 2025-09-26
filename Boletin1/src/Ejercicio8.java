cfb6718efba4aafe570bbc7b8543b5809c5cd94e
Carlos <elmejorgamer1205@gmail.com>
2025-09-26 10:15:38 +0200

    Primeros ficheros boletin 1

--- Boletin1/src/Ejercicio8.java
+++ Boletin1/src/Ejercicio8.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Ejercicio8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("¿Cuántos números quieres introduccir?");
+        System.out.println("Introduce un número");
         int n1 = sc.nextInt();
     }
 }
