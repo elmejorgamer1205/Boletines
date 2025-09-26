0a1d6e42b7c41589cb63fe0b890a29c485902326
Carlos <elmejorgamer1205@gmail.com>
2025-09-26 09:08:31 +0200

    Primeros ficheros boletin 1

--- Boletin1/src/Ejercicio7.java
+++ Boletin1/src/Ejercicio7.java
@@ -2,5 +2,24 @@ import java.util.Scanner;
 
 public class Ejercicio7 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¿Cuántos números quieres introduccir?");
+        int n1 = sc.nextInt();
+        double suma = 0;
+        int n2 = 0;
+
+        while (n1 <= 0) {
+            System.out.println("Error, itroduzca una cantida mayor a esa,¿Cuántos números quieres introduccir?");
+            n1 = sc.nextInt();
+        }
+        for (int i = 0; i < n1; i++) {
+            System.out.println("escrba sus números");
+            n2 = sc.nextInt();
+            suma += n2;
+        }
+        double media = suma / n1;
+        System.out.printf("La media de sus números es " + media);
     }
+
+
 }
