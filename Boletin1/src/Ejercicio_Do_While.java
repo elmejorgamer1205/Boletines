4f054e200a2dce6b6ec7606826cf0f950a9e1540
Carlos <elmejorgamer1205@gmail.com>
2025-09-26 10:08:44 +0200

    Primeros ficheros boletin 1

--- Boletin1/src/Ejercicio_Do_While.java
+++ Boletin1/src/Ejercicio_Do_While.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 public class Ejercicio_Do_While {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Introduzca una contraseña");
-        int contra = sc.nextInt();
+
 
     }
 }
