85ebd09ccf3ad964117b48100e30b3013c685c97
Carlos <elmejorgamer1205@gmail.com>
2025-09-30 12:30:35 +0200

    Primeros ficheros boletin 1

--- Boletin1/src/Ejercicio7.java
+++ Boletin1/src/Ejercicio7.java
@@ -6,7 +6,7 @@ public class Ejercicio7 {
         System.out.println("¿Cuántos números quieres introduccir?");
         int n1 = sc.nextInt();
         double suma = 0;
-        int n2 = 0;
+        int n2;
 
         while (n1 <= 0) {
             System.out.println("Error, itroduzca una cantida mayor a esa,¿Cuántos números quieres introduccir?");
