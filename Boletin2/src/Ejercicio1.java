9c9a8a16d4f9a926c4715b773587e76eeba40c1d
Carlos <elmejorgamer1205@gmail.com>
2025-10-01 09:31:58 +0200

    boletin 2

--- Boletin2/src/Ejercicio1.java
+++ Boletin2/src/Ejercicio1.java
@@ -1,7 +1,28 @@
 import java.util.Scanner;
 
 public class Ejercicio1 {
-   public static void main(String[] args){
+    public static void main(String[] args){
        Scanner sc = new Scanner(System.in);
+       System.out.println("Introduzca un número");
+       int num1 = sc.nextInt();
+       System.out.println("Introduzca un segundo número");
+       int num2 = sc.nextInt();
+       System.out.println("Introduzca un tercer número");
+       int num3 = sc.nextInt();
+
+       if((num1>num2) && (num2>num3)){
+           System.out.println("El orden de los números introducidos es "+ num1 +" "+ num2 +" "+ num3);
+       } else if((num1 > num3) && (num3 > num2)){
+           System.out.println("El orden correcto es " + num1 + " " + num3 + " " + num2);
+       } else if ((num2 > num1) && (num1 > num3)){
+           System.out.println("El orden correcto es " + num2 + " " + num1 + " " + num3);
+       } else if ((num2 > num3) && (num3 > num1)){
+           System.out.println("El orden correcto es " + num2 + " " + num3 + " " + num1);
+       } else if((num3 > num2) && (num2 > num1)){
+           System.out.println("El orden correcto es " + num3 + " " + num2 + " " + num1);
+       } else{
+           System.out.println("El orden correcto es " + num3 + " " + num1 + " " + num2);
+       }      
+
    }
 }
