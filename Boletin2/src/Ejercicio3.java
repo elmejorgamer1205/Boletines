d7f411eb288c07c0491a42feab55fe2df32c8682
Carlos <elmejorgamer1205@gmail.com>
2025-10-01 10:11:31 +0200

    boletin 2

--- Boletin2/src/Ejercicio3.java
+++ Boletin2/src/Ejercicio3.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public class Ejercicio3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca un número ");
+        int n1 = sc.nextInt();
+
+
     }
 }
