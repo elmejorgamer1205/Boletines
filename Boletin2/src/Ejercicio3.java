531037b9283171f29a93ce453a1b255053325dd0
Carlos <elmejorgamer1205@gmail.com>
2025-10-01 10:18:21 +0200

    boletin 2

--- Boletin2/src/Ejercicio3.java
+++ Boletin2/src/Ejercicio3.java
@@ -4,8 +4,13 @@ public class Ejercicio3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Introduzca un número ");
-        int n1 = sc.nextInt();
-
-
+        int num = sc.nextInt();
+        int n1 = num;
+        int caracteres = 0;
+        do{
+            n1 /=10;
+            caracteres++;
+        }while(n1>0);
+        System.out.println("El número "+num+" tiene "+caracteres+" cifras");
     }
 }
