791e49e6c7f1001ec056a0c6a18a4a51a5057832
elmejorgamer1205 <elmejorgamer1205@gmail.com>
2025-10-03 12:21:35 +0200

    boletin 2

--- Boletin2/src/Ejercicio4.java
+++ Boletin2/src/Ejercicio4.java
@@ -2,6 +2,25 @@ import java.util.Scanner;
 
 public class Ejercicio4 {
     public static void main(String[] args) {
+        int aleatorio = (int)(Math.random() * 100) + 1;
+        Scanner sc = new Scanner(System.in);
+        int intentos = 0;
+        boolean acertado = false;
+        while (intentos < MAX_INTENTOS && !acertado) {
+            System.out.println("Elige un número entre el 1 y el 100");
+            int a = sc.nextInt();
+            if(a > 100 || a <  1){
+                System.out.println("Este número no está permitido");
+            }else {
+                if(aleatorio < a) {
+                    System.out.println("El número "+a+" es mayor que el aleatorio");
+                }else if (aleatorio > a){
+                    System.out.println("El número "+a+" es menor que el aleatorio");
+                }else {
+                    System.out.println("Has acertado el número oculto "+ aleatorio);
+                }
+            }
+        }
 
 
     }
