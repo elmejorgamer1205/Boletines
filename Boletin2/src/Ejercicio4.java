eb72bee731ddebf6b2e91fa5188ce7ff7d856070
elmejorgamer1205 <elmejorgamer1205@gmail.com>
2025-10-03 12:31:40 +0200

    boletin 2

--- Boletin2/src/Ejercicio4.java
+++ Boletin2/src/Ejercicio4.java
@@ -1,27 +1,32 @@
 import java.util.Scanner;
 
+
 public class Ejercicio4 {
+    private static final int MAX_INTENTOS = 5;
     public static void main(String[] args) {
-        int aleatorio = (int)(Math.random() * 100) + 1;
+        int aleatorio = (int) (Math.random() * 100) + 1;
+        System.out.println(aleatorio);
         Scanner sc = new Scanner(System.in);
         int intentos = 0;
         boolean acertado = false;
-        while (intentos < MAX_INTENTOS && !acertado) {
+        while (intentos != MAX_INTENTOS && !acertado) {
             System.out.println("Elige un número entre el 1 y el 100");
             int a = sc.nextInt();
-            if(a > 100 || a <  1){
+            if (a > 100 || a < 1) {
                 System.out.println("Este número no está permitido");
-            }else {
-                if(aleatorio < a) {
-                    System.out.println("El número "+a+" es mayor que el aleatorio");
-                }else if (aleatorio > a){
-                    System.out.println("El número "+a+" es menor que el aleatorio");
-                }else {
-                    System.out.println("Has acertado el número oculto "+ aleatorio);
+                intentos++;
+
+            } else {
+                if (a < aleatorio) {
+                    System.out.println("El número " + a + " es menor que el aleatorio");
+                } else if (a > aleatorio) {
+                    System.out.println("El número " + a + " es mayor que el aleatorio");
+                } else {
+                    System.out.println("Has acertado el número oculto " + aleatorio);
+                    acertado = true;
                 }
             }
+            ++intentos;
         }
-
-
     }
 }
