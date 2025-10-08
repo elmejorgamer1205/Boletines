7502fae600a6739300804d8287afbdda823ddae6
elmejorgamer1205 <elmejorgamer1205@gmail.com>
2025-10-08 10:13:34 +0200

    Boletín 2.2

--- Boletin2.2/src/Ejercicio3.java
+++ Boletin2.2/src/Ejercicio3.java
@@ -2,7 +2,18 @@ import java.util.Scanner;
 
 public class Ejercicio3 {
     public static final int M=1000, D=500, C=100, L=50, X=10, V=5, I=1;
-    Scanner sc = new Scanner(System.in);
+
+    public static void main(String[]args){
+        int resultado = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce tu número Romano");
+        for (int i = 0; i < ; i++) {
+
+        }
+
+    }
+
+
 
 
 }
