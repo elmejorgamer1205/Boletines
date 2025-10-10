374a80730587301cd859da5b18ba5dacb4179e6f
elmejorgamer1205 <elmejorgamer1205@gmail.com>
2025-10-10 10:15:27 +0200

    Boletin arrays

--- BoletinArrays1/src/Ejercicio1.java
+++ BoletinArrays1/src/Ejercicio1.java
@@ -1,2 +1,12 @@
 public class Ejercicio1 {
+    public static void main(String[] args){
+        int[] x = {1,2,3,4,5};
+        int suma = 0;
+        for (int i = 0; i < x.length; i+=2) {
+            suma += x[i];
+
+        }
+        System.out.printf("La suma de los números pares del array es %d",suma);
+        //Recorres el array pero sólo las posiciones pares, y sumar el contenido a suma
+    }
 }
---
+++ BoletinArrays1/src/Ejercicio2.java
@@ -0,0 +1,6 @@
+public class Ejercicio2 {
+    public static void main (String[]args){
+        int[] x = {-1,2,-3,4,-5};
+
+    }
+}
