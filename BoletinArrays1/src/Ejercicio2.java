9aaa99ba13174b3e22ed1059333d739a0dcbfd9d
elmejorgamer1205 <elmejorgamer1205@gmail.com>
2025-10-14 10:38:34 +0200

    Boletin arrays

--- BoletinArrays1/src/Ejercicio2.java
+++ BoletinArrays1/src/Ejercicio2.java
@@ -1,6 +1,16 @@
 public class Ejercicio2 {
-    public static void main (String[]args){
-        int[] x = {-1,2,-3,4,-5};
+    public static void main(String[] args) {
+        int[] x = {-1, 2, -3, 4, -5};
+        int contador = 0;
+        /*for (int i = 0; i < x.length; i++) {
+            if (x[i] < 0) {
+                contador++;
+            }
+        }*/
+        for (int numero : x) {
+            if (numero < 0);
+            contador++;
+            }
 
     }
 }
