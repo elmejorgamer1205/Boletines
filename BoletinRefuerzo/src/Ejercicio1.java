ad03c80ce0d5fb9ab786b69df98310b857d77d3b
Carlos home <elmejorgamer1205@gmail.com>
2025-10-14 18:40:54 +0200

    Refuerzo 1

--- BoletinRefuerzo/src/Ejercicio1.java
+++ BoletinRefuerzo/src/Ejercicio1.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 
 public class Ejercicio1 {
     public static void main(String[] args){
+        //Creo un Scanner para preguntar al usuario los números que quiere sumar
         Scanner sc = new Scanner(System.in);
         System.out.println("Escriba los números que quiere sumar:");
+        //Creo las variables a para el primer número y la variable b para el segundo número recogido por pantalla
         int a = sc.nextInt();
         int b = sc.nextInt();
+        // Creo una variable que sume a y b, y guarde el resultado
         int suma = a + b;
+        //Imprimo por pantalla el resultado de la suma
         System.out.println("La suma de "+a+" con "+b+" es: "+suma);
     }
 }
--- BoletinRefuerzo/src/Ejercicio2.java
+++ BoletinRefuerzo/src/Ejercicio2.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class Ejercicio2 {
+    public static void main(String[] args) {
+        //Creo un escaner para preguntar al usuario el número binario que quiere hacer la conversión
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escriba su número binario: ");
+        //Creo una variable para recoger el numero binario del usuario
+        int numBinario = sc.nextInt();
+        //Creo la variable numDecimal para recoger el resultado de hacer la conversión
+        int numDecimal = 0;
+        //Creo un bucle While ya que como no sé cuantas veces puede llegar el usuario a fallar
+        //Creado una vez el bucle, le digo que si los números son mayores que 1 o menores que 0 le mande un mensaje diciendo que solo se puede poner los números 0 y 1
+        while (numBinario < 0 || numBinario > 1) {
+            System.out.println("introduce solo los números 1 y 0 para la conversión");
+            numBinario = sc.nextInt();
+        }
+        //Creo un bucle For porque como el número ya esta establecido por el usario y no lo puede cambiar puedo recorrer una por una cada sección de ese número para hacer el cálculo
+        for (int i = 0; i < numBinario; i++) {
+            numDecimal= numBinario*10;
+            System.out.println("Tú número binario sería: "+numDecimal);
+        }
+    }
 }
+
