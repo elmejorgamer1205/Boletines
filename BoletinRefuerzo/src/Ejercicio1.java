e5137ce349a5f7c008ee186734fc12af4466e8db
Carlos home <elmejorgamer1205@gmail.com>
2025-10-14 18:08:12 +0200

    Refuerzo 1

--- BoletinRefuerzo/src/Ejercicio1.java
+++ BoletinRefuerzo/src/Ejercicio1.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class Ejercicio1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escriba los números que quiere sumar:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int suma = a + b;
+        System.out.println("La suma de "+a+" con "+b+" es: "+suma);
+    }
 }
