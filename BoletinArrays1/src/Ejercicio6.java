3d0be0a4b4466c86c300e044921b4fdd05120a84
elmejorgamer1205 <elmejorgamer1205@gmail.com>
2025-10-17 09:16:11 +0200

    boletín Arrays

--- BoletinArrays1/src/Ejercicio6.java
+++ BoletinArrays1/src/Ejercicio6.java
@@ -1,18 +1,19 @@
 public class Ejercicio6 {
-    public static void main(String[] args){
-        int[] miArray = {1,2,3,4,5};
-        if (contieneElemento(miArray, 7) ){
+    public static void main(String[] args) {
+        int[] miArray = {1, 2, 3, 4, 5};
+        if (contieneElemento(miArray, 7)) {
             System.out.println("El elemento esta contenido");
-        }else {
+        } else {
             System.out.println("El elemento no esta contenido");
         }
     }
-    public static boolean contieneElemento (int[] array, int elementoABuscar){
+
+    public static boolean contieneElemento(int[] array, int elementoABuscar) {
         for (int i = 0; i < array.length; i++) {
-            if( array[i] == elementoABuscar){
+            if (array[i] == elementoABuscar) {
                 return true;
             }
         }
         return false;
     }
-
+}
