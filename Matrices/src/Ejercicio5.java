8be8bdc123a3f36a8e40bad78b38472993900130
elmejorgamer1205 <elmejorgamer1205@gmail.com>
2025-10-23 13:18:11 +0200

    Matrices

---
+++ Matrices/src/Ejercicio5.java
@@ -0,0 +1,14 @@
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        int[][] matriz = {{1, 3, 5}, {0, 2, 7}};
+        ImprimirEnUnaSolaLinia(matriz);
+    }
+
+    public static void ImprimirEnUnaSolaLinia(int[][] matriz) {
+        for (int j = 0; j < matriz[0].length; j++) {
+            for (int i = 0; i < matriz.length; i++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+        }
+    }
+}
