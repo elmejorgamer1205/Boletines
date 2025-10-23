aff780ea4dfdb9dd2aada90c3fba653725a22206
elmejorgamer1205 <elmejorgamer1205@gmail.com>
2025-10-23 13:40:17 +0200

    Matrices

---
+++ Matrices/src/Ejercicio7.java
@@ -0,0 +1,27 @@
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        int[][] matriz1 = {{1, 2, 3}, {4, 5, 6}};
+        int[][] matriz2 = {{1, 2, 3}, {4, 5, 6}};
+
+        System.out.println(comprobacionSiSonIguales(matriz1,matriz2));
+    }
+
+    public static boolean comprobacionSiSonIguales(int[][] matriz1, int[][] matriz2) {
+        if (matriz1.length != matriz2.length) {
+            return false;
+        }
+        for (int i = 0; i < matriz1.length; i++) {
+            if (matriz1[i].length != matriz2[i].length) {
+                return false;
+            }
+        }
+        for (int i = 0; i < matriz1.length; i++) {
+            for (int j = 0; j < matriz1[i].length; j++) {
+                if (matriz1[i][j] != matriz2[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
