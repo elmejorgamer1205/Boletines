0e6194113214c9c89639a7b15013cf0e3db99e03
elmejorgamer1205 <elmejorgamer1205@gmail.com>
2025-10-23 12:25:48 +0200

    Matrices

--- Matrices/src/Ejercicio1.java
+++ Matrices/src/Ejercicio1.java
@@ -1,18 +1,22 @@
 public class Ejercicio1 {
-    public static void main(String[] args){
-        int[][] matriz = {{1,-2,3,-4,5}, {-1,2,-3,4,5}};
+    public static void main(String[] args) {
+        int[][] matriz = {{1, -2, 3, 4, 5}, {1, 2, 3, 4, 5}};
+        if (comprobarNegeativos(matriz)) {
+            System.out.print("En la matriz si hay un valor negativo");
+        } else
+            System.out.print("En la matriz no hay un valor negativo");
+
 
     }
-    public static boolean comprobarNegeativos (int[][]matriz){
+
+    public static boolean comprobarNegeativos(int[][] matriz) {
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
-                if (comprobarNegeativos(matriz)){
-                    int matriz2 = matriz[i].length;
-
+                if (matriz[i][j] < 0) {
+                    return true;
                 }
-
             }
-
         }
+        return false;
     }
 }
