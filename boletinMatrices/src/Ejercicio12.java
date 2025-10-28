f252d0172564355d3b3fa8c35130caf29bb6eef5
elmejorgamer1205 <elmejorgamer1205@gmail.com>
2025-10-28 12:42:54 +0100

    Matrices

---
+++ boletinMatrices/src/Ejercicio12.java
@@ -0,0 +1,10 @@
+public class Ejercicio12 {
+    public static void main(String[] args){
+        int[][] matriz = {{1,2,3,4,5},{6,7,8,9,0},{3,2,1,4,5},{9,5,8,6,7},{0,9,8,5,4}};
+    }
+    public static int[][] reflejoDiagonal(int[][] matriz){
+        int[][] reflejado;
+        int[][] error = {{-1}};
+
+    }
+}
