1a3e59ffd6557205d876114298146bfd14ac7e3c
elmejorgamer1205 <elmejorgamer1205@gmail.com>
2025-10-29 08:34:50 +0100

    Matrices

--- boletinMatrices/src/Ejercicio12.java
+++ boletinMatrices/src/Ejercicio12.java
@@ -1,10 +1,29 @@
 public class Ejercicio12 {
-    public static void main(String[] args){
-        int[][] matriz = {{1,2,3,4,5},{6,7,8,9,0},{3,2,1,4,5},{9,5,8,6,7},{0,9,8,5,4}};
-    }
-    public static int[][] reflejoDiagonal(int[][] matriz){
-        int[][] reflejado;
-        int[][] error = {{-1}};
+    public static boolean matrizCuadradaTranspuesta(int[][] matriz) {
+        if (matriz.length != matriz[0].length) {
+            return false;
+        }
 
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.print(matriz[j][i] + " ");
+            }
+            System.out.println();
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int[][] matriz1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] matriz2 = {{1, 2, 3}, {4, 5, 6}};
+
+        System.out.println("Matriz 1 transpuesta:");
+        if (!matrizCuadradaTranspuesta(matriz1)) {
+            System.out.println("La matriz no es cuadrada.");
+        }
+
+        System.out.println("Matriz 2 transpuesta:");
+        if (!matrizCuadradaTranspuesta(matriz2)) {
+            System.out.println("La matriz no es cuadrada.");
+        }
     }
 }
