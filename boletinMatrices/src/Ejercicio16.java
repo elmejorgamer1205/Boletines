0e80cf9f77e40eedf8cd2a51a4ed0103cbb0dcd0
elmejorgamer1205 <elmejorgamer1205@gmail.com>
2025-10-30 12:13:03 +0100

    Matrices

--- boletinMatrices/src/Ejercicio16.java
+++ boletinMatrices/src/Ejercicio16.java
@@ -1,27 +1,37 @@
 public class Ejercicio16 {
-    public static void main(String[] args){
-        int[][] matriz = {{1,2,3,4,5},{6,7,8,9,0},{3,2,1,4,5},{9,5,8,6,7},{0,9,8,5,4}};
+    public static void main(String[] args) {  //Ejemplo de uso que imprime la matriz resultado
+        int[][] matriz = {{1, 0, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+        int[][] resultado = generarMatrizResultado(matriz);
+        for (int i = 0; i < resultado.length; i++) {
+            for (int j = 0; j < resultado[0].length; j++) {
+                System.out.print(resultado[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
-    public static int[][] comprobarMatrizEnteros(int[][] matriz){
-        int[][] nuevaMatriz = new int[matriz.length][matriz[0].length];
-        int[] dx = {-1,-1,-1,0,0,1,1,1};
-        int[] dy = {-1,0,1,-1,1,1,0,1};
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                int contador = 0;
-                for (int k = 0; k < 8; k++) {
-                    int ni = i + dx[k];
-                    int nj = j + dy[k];
-                    if (ni >= 0 && ni < matriz.length && nj >= 0 && nj < matriz[0].length) {
-                        if (matriz[ni][nj] % 2 == 0) {
-                            contador++;
-                        }
-                    }
+    public static boolean esPar(int valor) {  //Función para determinar si un número es par
+        return valor % 2 == 0;
+    }
+    public static int contarAdyacentesPares(int[][] matriz, int fila, int col) {  //Función para contar números pares adyacentes
+        int contador = 0;
+        for (int i = fila - 1; i <= fila + 1; i++) {  //Dentro del rango de filas y columnas detecta las adyacentes incluidas las diagonales
+            for (int j = col - 1; j <= col + 1; j++) {
+                if (i == fila && j == col) continue;
+                if (i >= 0 && i < matriz.length && j >= 0 && j < matriz[0].length) {
+                    if (esPar(matriz[i][j])) contador++;
                 }
-                nuevaMatriz[i][j] = contador;
             }
         }
-        return nuevaMatriz;
+        return contador;
+    }
+    public static int[][] generarMatrizResultado(int[][] matriz) {  //Función para generar la matriz resultado
+        int[][] resultado = new int[matriz.length][matriz[0].length];
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                resultado[i][j] = contarAdyacentesPares(matriz, i, j);
+            }
+        }
+        return resultado;
     }
 }
 
