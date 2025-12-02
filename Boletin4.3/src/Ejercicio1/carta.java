70655d492101e9b1d9289ce5f0f025649d4fc25f
elmejorgamer1205 <elmejorgamer1205@gmail.com>
2025-12-02 12:41:18 +0100

    Boletín 4.3

--- Boletin4.3/src/Ejercicio1/carta.java
+++ Boletin4.3/src/Ejercicio1/carta.java
@@ -1,4 +1,57 @@
 package Ejercicio1;
 
+import Exceptions.cartaException;
+
 public class carta {
+
+    private String palo;
+    private String número;
+
+
+    public carta() {
+        setNúmero(número);
+        setPalo(palo);
+    }
+
+    public String getNúmero() {
+        return número;
+    }
+
+    public void setNúmero(String número) {
+        this.número = número;
+    }
+
+    public String getPalo() {
+        return palo;
+    }
+
+    public void setPalo(String palo) {
+        this.palo = palo;
+    }
+
+    public void crearBaraja(){
+
+    }
+
+    public void mostrarBaraja() throws cartaException{
+        System.out.println(toString());
+    }
+
+    public void barajarBaraja() throws cartaException{
+
+    }
+
+    public void repartiCartas() throws cartaException{
+
+    }
+
+    public void cartasRestantes() throws cartaException{
+
+    }
+
+    @Override
+
+    public String toString(){
+        return "";
+    }
 }
---
+++ Boletin4.3/src/Exceptions/cartaException.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class cartaException extends Exception {
+    public cartaException(String message) {
+        super(message);
+    }
+}
