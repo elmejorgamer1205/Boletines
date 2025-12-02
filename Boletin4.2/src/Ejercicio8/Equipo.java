b121bde0ff586e6f879b5b2d123c0e090f6919fc
elmejorgamer1205 <elmejorgamer1205@gmail.com>
2025-12-02 12:17:40 +0100

    Boletín 4.3

--- Boletin4.2/src/Ejercicio8/Equipo.java
+++ Boletin4.2/src/Ejercicio8/Equipo.java
@@ -2,4 +2,50 @@ package Ejercicio8;
 
 public class Equipo {
     private String nombre;
+    private int partidosGanados;
+    private String estadio;
+    private String ciudad;
+
+    public Equipo(String nombre, int partidosGanados, String estadio, String ciudad) {
+        setNombre(nombre);
+        setPartidosGanados(partidosGanados);
+        setEstadio(estadio);
+        setCiudad(ciudad);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPartidosGanados() {
+        return partidosGanados;
+    }
+
+    private void setPartidosGanados(int partidosGanados) {
+        this.partidosGanados = partidosGanados;
+    }
+
+    public String getEstadio() {
+        return estadio;
+    }
+
+    public void setEstadio(String estadio) {
+        this.estadio = estadio;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public void sumarPartidoGanados(int suma){
+        partidosGanados+=suma;
+    }
 }
--- Boletin4.2/src/Ejercicio8/Partido.java
+++ Boletin4.2/src/Ejercicio8/Partido.java
@@ -2,17 +2,18 @@ package Ejercicio8;
 
 import Exceptions.PartidoExceptions;
 
-public class Partido{
-
+public class Partido {
     private Equipo equipoLocal;
-    private Equipo  equipoVisitante;
+    private Equipo equipoVisitante;
     private int jornada;
     private int golesEquipoLocal;
     private int golesEquipoVisitante;
-    private  boolean jugado;
+    private boolean jugado;
     private char quiniela;
 
-    public Partido(Equipo equipoLocal, Equipo equipoVisitante, int jornada, int golesEquipoLocal, int golesEquipoVisitante, boolean jugado, char quiniela){
+
+    public Partido(Equipo equipoLocal, Equipo equipoVisitante, int jornada, int golesEquipoLocal,
+                   int golesEquipoVisitante, boolean jugado, char quiniela) {
         this.equipoLocal = equipoLocal;
         this.equipoVisitante = equipoVisitante;
         this.jornada = jornada;
@@ -20,32 +21,47 @@ public class Partido{
         this.golesEquipoVisitante = golesEquipoVisitante;
         this.jugado = jugado;
         this.quiniela = quiniela;
-
     }
 
-    public void  ponerResultado(String resultado) throws PartidoExceptions {
-        String[] goles = resultado.split("-");
-        int[] numGoles = new int[goles.length];
-        for (int i = 0; i < goles.length; i++) {
-            numGoles[i] = Integer.parseInt(goles[i]);
-        }
-
-        if(numGoles[0] < 0 || numGoles[1] <0){
+    public void ponerResultado(String resultado) throws PartidoExceptions {
+        String[] goles = resultado.split("-",2);
+        int[] numGoles = new int[] {
+                Integer.parseInt(goles[0]),
+                Integer.parseInt(goles[1])
+        };
+        if (numGoles[0]<numGoles[1]){
             this.quiniela='1';
-            golesEquipoLocal = numGoles[0];
-            golesEquipoVisitante = numGoles[1];
-
+            golesEquipoLocal=numGoles[0];
+            golesEquipoVisitante=numGoles[1];
+            jugado=true;
+            equipoLocal.sumarPartidoGanados(1);
         }
-        else if (numGoles [0] == numGoles[1]){
+        else if (numGoles[0]==numGoles[1]){
             this.quiniela='X';
-            golesEquipoVisitante = numGoles[0];
-            golesEquipoLocal = numGoles [0];
+            golesEquipoLocal=numGoles[0];
+            golesEquipoVisitante=numGoles[1];
+            jugado=true;
         }else {
-            this.quiniela = '2';
-            golesEquipoLocal = numGoles[1];
-            golesEquipoVisitante = numGoles[0];
-            jugado = true;
+            this.quiniela='2';
+            golesEquipoLocal=numGoles[0];
+            golesEquipoVisitante=numGoles[1];
+            jugado=true;
+            equipoVisitante.sumarPartidoGanados(1);
         }
     }
 
+    @Override
+    public String toString() {
+        if (!jugado) {
+            return "Partido{" +
+                    "Partido entre equipo local" + equipoLocal.getNombre() +
+                    "y equipo visitante" + equipoVisitante.getNombre() +
+                    "todavía no se ha jugado”\n";
+        }else
+            return "Partido{" +
+                    "Partido entre equipo local" + equipoLocal.getNombre() +
+                    "y equipo visitante" + equipoVisitante.getNombre() +
+                    "jugado en el estadio" +
+                    " \n";
+    }
 }
