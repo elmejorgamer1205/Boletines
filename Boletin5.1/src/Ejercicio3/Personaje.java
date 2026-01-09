11b9ab0dbb9e66f0f6fd6e4762736d3ab573c177
elmejorgamer1205 <elmejorgamer1205@gmail.com>
2026-01-09 08:39:25 +0100

    Boletín 5.1

--- Boletin5.1/src/Ejercicio3/Personaje.java
+++ Boletin5.1/src/Ejercicio3/Personaje.java
@@ -7,25 +7,55 @@ public class Personaje {
     private Raza raza;
     private int fuerza;
     private int inteligencia;
-    private int VIDA_MAX;
-    private int vidaActu;
+    private final int VIDA_MAX;
+    private int vidaActual;
 
-    public Personaje(String nombre, String[] raza, int fuerza, int inteligencia, int vidaMax, int vidaActu)throws PersonajeException {
-       this.nombre = nombre;
-       setRaza(raza);
-       setFuerza(fuerza);
-       setInteligencia(inteligencia);
+    public Personaje(String nombre, String raza, int fuerza, int inteligencia, int vidaMax) throws PersonajeException {
+        this.nombre = nombre;
+        setRaza(raza);
+        setFuerza(fuerza);
+        setInteligencia(inteligencia);
+        if (vidaMax < 0 || vidaMax > 100) {
+            throw new PersonajeException("La vida máxima introducida no es válida.");
+        }
+        this.VIDA_MAX = vidaMax;
+        this.vidaActual=VIDA_MAX;
     }
 
-    public void setRaza(String raza) {
-        this.raza = raza;
+    private void setRaza(String raza) {
+        this.raza = Raza.valueOf(raza.toUpperCase());
     }
 
-    public void setFuerza(int fuerza) {
+    private void setFuerza(int fuerza)throws PersonajeException {
+        if (fuerza<0 || fuerza>20){
+            throw new PersonajeException("La fuerza introducida no es válida");
+        }
         this.fuerza = fuerza;
     }
 
-    public void setInteligencia(int inteligencia) {
+    private void setInteligencia(int inteligencia)throws PersonajeException {
+        if (inteligencia<0 || inteligencia>20){
+            throw new PersonajeException("La inteligencia introducida no es válida");
+        }
         this.inteligencia = inteligencia;
+    }
+
+    public void modificarVidaActual(int modificacion) throws PersonajeException {
+        if (vidaActual+modificacion>VIDA_MAX){
+            throw new PersonajeException("Este personaje no puede ser curado, su vida esta al máximo");
+        }
+        this.vidaActual += modificacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Personaje{" +
+                "nombre='" + nombre + '\'' +
+                ", raza=" + raza +
+                ", fuerza=" + fuerza +
+                ", inteligencia=" + inteligencia +
+                ", VIDA_MAX=" + VIDA_MAX +
+                ", vidaActual=" + vidaActual +
+                '}';
     }
 }
