40a5452bc33ed1580afd3802dfc05edbf106483a
elmej <elmejorgamer1205@gmail.com>
2026-02-09 20:16:48 +0100

    commit

--- JuegoPokemon/src/interfaces/Atacador.java
+++ JuegoPokemon/src/interfaces/Atacador.java
@@ -1,4 +1,18 @@
 package interfaces;
 
+import enums.WeatherCondition;
+import exceptions.MuerteException;
+
 public interface Atacador {
+
+    /**
+     * Método de la interfaz para bajar la vida a tus contringantes y ganarles en tus peleas
+     * Pokemón
+     *
+     * @param objetivo parametro para seleccionar al pokemon que quieres atacar
+     * @param tiempo al usar este parámetro nos ayuda si un Pokémon recibe más o menos daño dependiendo
+     *               que tiempo meteorologico hace en batalla.
+     * @throws MuerteException hace que si el Pokémon que atacaste tenga de vida actual 0 muestre por pantalla que esté derrotado
+     */
+    void atacar(Atacable objetivo, WeatherCondition tiempo) throws MuerteException;
 }
