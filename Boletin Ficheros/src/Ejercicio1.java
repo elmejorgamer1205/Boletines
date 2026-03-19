0cf2677c9a52d16dd32b1d6a99dfc9aa3cfcd1cc
elmejorgamer1205 <elmejorgamer1205@gmail.com>
2026-03-19 13:39:22 +0100

    commit

--- Boletin Ficheros/src/Ejercicio1.java
+++ Boletin Ficheros/src/Ejercicio1.java
@@ -1,16 +1,35 @@
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Ejercicio1 {
     static void main(String[] args) throws FileNotFoundException {
         File f = new File("leeme.txt");
 
         try (FileReader fr = new FileReader(f);
-        BufferedReader bre = new BufferedReader(fr)){
+            BufferedReader br = new BufferedReader(fr)){
+            /* Primera forma:
+
+            int contador = 0;
+
+            while(br.readLine() != null){
+                contador++;
+            }
+
+            System.out.printf("Había %d líneas", contador);
+            */
+
+            /*Segunda forma:
+
+            System.out.printf("Había %d líneas", br.lines().count());
+            */
+
+            Path fichero = Path.of("./src/leeme.txt");
+
+            System.out.printf("Había %d líneas", Files.lines(fichero).count());
 
         }catch (IOException e){
             System.out.println(e.getMessage());
         }
-
-
     }
 }
