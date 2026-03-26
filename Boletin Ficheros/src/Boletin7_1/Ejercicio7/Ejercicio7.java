39f1eaefc5883d552bb6369c18e9619aa17e47d5
elmejorgamer1205 <elmejorgamer1205@gmail.com>
2026-03-26 12:32:10 +0100

    commit

--- Boletin Ficheros/src/Boletin7_1/Ejercicio7/Ejercicio7.java
+++ Boletin Ficheros/src/Boletin7_1/Ejercicio7/Ejercicio7.java
@@ -1,9 +1,17 @@
-package Boletin7_1.Ejercicio7;
+package ejercicio7;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class Ejercicio7 {
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+
         int opcion;
         do {
             mostrarMenu();
@@ -13,19 +21,29 @@ public class Ejercicio7 {
 
             switch (opcion) {
                 case 1:
-                    System.out.println("Opción 2: Listar con prefijo");
+                    opcion1();
+                    System.out.println("Opción 1: Listar directorio");
                     break;
                 case 2:
-                    System.out.println("Opción 3: Listar por extensión");
+                    System.out.println("Opción 2: Listar con prefijo");
+                    opcion2();
                     break;
                 case 3:
-                    System.out.println("Opción 4: Buscar archivo simple");
+                    System.out.println("Opción 3: Listar por extensión");
+                    opcion3();
                     break;
                 case 4:
-                    System.out.println("Opción 5: Buscar recursivo");
+                    System.out.println("Opción 4: Buscar archivo simple");
+                    opcion4();
                     break;
                 case 5:
+                    System.out.println("Opción 5: Buscar recursivo");
+                    //La vamos a utilizar con Files.walk
+                    opcion5();
+                    break;
+                case 6:
                     System.out.println("¡Saliendo del programa!");
+
                     break;
                 default:
                     System.out.println("Opción inválida");
@@ -45,8 +63,121 @@ public class Ejercicio7 {
         System.out.println("=====================================");
     }
 
-    public static void opcion1(){
+    public static void opcion1() {
+
+    }
+
+    public static void opcion2() {
+
         System.out.println("Introduce el directorio: \n");
-        Path p = Path.of()
+        Path p = Path.of(scanner.nextLine());
+
+        System.out.println("Introduce por cual palabra quieres buscar");
+        String palabraABuscar = scanner.nextLine();
+        if (Files.isDirectory(p)) {
+            try (Stream<Path> ficheros = Files.list(p)) {
+                ficheros.filter(path -> path.getFileName().toString().startsWith(palabraABuscar))
+                        .forEach(path -> {
+                            if (Files.isDirectory(path)) {
+                                System.out.printf("%s - directorio %n", path.getFileName());
+                            } else {
+                                try {
+                                    System.out.printf("%s %.2f kb %n", path.getFileName(), Files.size(path) / 1024.0);
+                                } catch (IOException e) {
+                                    System.out.println(e.getMessage());
+                                }
+                            }
+                        });
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+
+        }
+    }
+
+    public static void opcion3() {
+        System.out.println("Introduce el directorio: \n");
+        Path p = Path.of(scanner.nextLine());
+
+        System.out.println("Introduce por cual extension quieres buscar (sin el punto)");
+        String extensionABuscar = scanner.nextLine();
+        if (Files.isDirectory(p)) {
+
+            try (Stream<Path> ficheros = Files.list(p)) {
+                ficheros.filter(path -> path.getFileName().toString().endsWith("." + extensionABuscar))
+                        .forEach(path -> {
+                            if (Files.isDirectory(path)) {
+                                System.out.printf("%s - directorio %n", path.getFileName());
+                            } else {
+                                try {
+                                    System.out.printf("%s %.2f kb %n", path.getFileName(), Files.size(path) / 1024.0);
+                                } catch (IOException e) {
+                                    System.out.println(e.getMessage());
+                                }
+                            }
+                        });
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+
+        }
+    }
+
+    public static void opcion4() {
+        System.out.println("Introduce el directorio: \n");
+        Path p = Path.of(scanner.nextLine());
+
+        System.out.println("Introduce el nombre del fichero que quieres buscar");
+        String ficheroABuscar = scanner.nextLine();
+
+        if (Files.isDirectory(p)) {
+
+            try (Stream<Path> ficheros = Files.find(p, 1, ((path, atr) -> {
+                return path.getFileName().toString().equals(ficheroABuscar);
+            }))) {
+                ficheros.forEach(path -> {
+                    if (Files.isDirectory(path)) {
+                        System.out.printf("%s - directorio %n", path.getFileName());
+                    } else {
+                        try {
+                            System.out.printf("%s %.2f kb %n", path.getFileName(), Files.size(path) / 1024.0);
+                        } catch (IOException e) {
+                            System.out.println(e.getMessage());
+                        }
+                    }
+                });
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+
+        }
+    }
+
+
+    public static void opcion5() {
+        System.out.println("Introduce el directorio \n");
+        Path p = Path.of(scanner.nextLine());
+
+        System.out.println("Introduce el nombre del archivo a buscar");
+        String archivoABuscar = scanner.nextLine();
+
+        if (Files.isDirectory(p)) {
+            try (Stream<Path> ficheros = Files.walk(p)) {
+                ficheros.filter(path -> path.getFileName().toString().contains(archivoABuscar))
+                        .forEach(path -> {
+                            if (Files.isDirectory(path)) {
+                                System.out.printf("%s - directorio %n", path.getFileName());
+                            } else {
+                                try {
+                                    System.out.printf("%s %.2f kb %n", path.getFileName(), Files.size(path) / 1024.0);
+                                } catch (IOException e) {
+                                    System.out.println(e.getMessage());
+                                }
+                            }
+                        });
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
