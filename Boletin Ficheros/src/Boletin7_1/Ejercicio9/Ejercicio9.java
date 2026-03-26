67c869df0972157ba2f8db972992413b0afcfde1
elmejorgamer1205 <elmejorgamer1205@gmail.com>
2026-03-26 13:47:54 +0100

    commit

--- Boletin Ficheros/src/Boletin7_1/Ejercicio9/Ejercicio9.java
+++ Boletin Ficheros/src/Boletin7_1/Ejercicio9/Ejercicio9.java
@@ -1,31 +1,31 @@
-package Boletin7_1.Ejercicio8;
+package Boletin7_1.Ejercicio9;
 
 import io.MiEntradaSalida;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.io.IOException;
+import java.nio.file.StandardOpenOption;
 import java.util.stream.Stream;
 
-public class Ejercicio8 {
+public class Ejercicio9 {
 
     static void main(String[] args) {
 
         String nombreFichero = MiEntradaSalida.leerLinea("Introduce el nombre del fichero: ");
 
 
-        Path path = Path.of("src", "Boletin7.1","Ejercicio8", nombreFichero);
+        Path path = Path.of("src", "Boletin7.1","Ejercicio9", nombreFichero);
 
         if (!Files.exists(path) || !Files.isRegularFile(path)) {
             System.err.println("El fichero no existe o no es valido.");
             return;
         }
         try (Stream<String> lineas = Files.lines(path)){
+            lineas.filter(l -> l.matches("/\\p{L}+\\s\\d{4}-[B-Z&&[^EIOU]]{3}"));
 
-            boolean cumple = lineas.allMatch(l -> l.matches("(\\p{L}{2,}\\s){3}([1-9]|([1-9][0-9]))$"));
-            if (cumple) System.out.println("cumple");
-            else System.out.println("no cumple");
-
+            Files.writeString(StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+            
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
